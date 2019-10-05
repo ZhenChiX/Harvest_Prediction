cc260410bef2239020467398270c82854e6a86a6
StarGale <zhenchix@hotmail.com>
2019-10-04 20:54:38 -0700

    babel polyfill

--- harvest-prediction/src/index.js
+++ harvest-prediction/src/index.js
@@ -1,10 +1,11 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import "@babel/polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
